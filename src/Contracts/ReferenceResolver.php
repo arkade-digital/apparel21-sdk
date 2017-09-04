cb88f14af4292317b069e3b9d1d41e7300b643dd
Dan Greaves <dan@arkade.com.au>
2017-09-04 16:49:56 +1000

    Update Referenceresolver contract

--- src/Contracts/ReferenceResolver.php
+++ src/Contracts/ReferenceResolver.php
@@ -3,9 +3,15 @@
 namespace Arkade\Apparel21\Contracts;
 
 use Arkade\Apparel21\Entities;
-use Illuminate\Support\Collection;
 
 interface ReferenceResolver
 {
-    //
+    /**
+     * Attempt to resolve a reference from the provided IDs.
+     *
+     * @param  string $typeId
+     * @param  string $referenceId
+     * @return Entities\Reference|null
+     */
+    public function resolveFromIds($typeId, $referenceId);
 }
