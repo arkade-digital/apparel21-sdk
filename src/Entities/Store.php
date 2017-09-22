18db7769209accd45ec157f17ee5b23ba01a80fd
Dan Greaves <dan@arkade.com.au>
2017-09-22 17:31:17 +1000

    Refactor GetFreeStock action

--- src/Entities/Store.php
+++ src/Entities/Store.php
@@ -2,11 +2,40 @@
 
 namespace Arkade\Apparel21\Entities;
 
-use Arkade\Support\Contracts;
 use Arkade\Support\Traits;
+use Arkade\Support\Contracts;
 
-class Store implements Contracts\Store, Contracts\Identifiable
+class Store implements Contracts\Identifiable
 {
-    use Traits\Store;
     use Traits\Identifiable;
+
+    /**
+     * Name of store.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Get name of store.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name of store.
+     *
+     * @param  string $name
+     * @return static
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
---
+++ src/Parsers/FreeStockParser.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Arkade\Apparel21\Parsers;
+
+use SimpleXMLElement;
+use Arkade\Apparel21\Entities;
+use Arkade\Apparel21\Exceptions;
+use Illuminate\Support\Collection;
+
+class FreeStockParser
+{
+    /**
+     * Parse the given SimpleXmlElement to a collection of FreeStock entities.
+     *
+     * @param  SimpleXMLElement $payload
+     * @return Collection
+     * @throws Exceptions\Apparel21Exception
+     */
+    public function parse(SimpleXMLElement $payload)
+    {
+        if ('Style' === $payload->getName()) {
+            return $this->parseStyleResponse($payload);
+        }
+
+        if ('Clr' === $payload->getName()) {
+            return $this->parseClrResponse($payload);
+        }
+
+        if ('Sku' === $payload->getName()) {
+            return $this->parseSkuResponse($payload);
+        }
+
+        throw new Exceptions\Apparel21Exception(sprintf(
+            'Unrecognised response lookup root %s',
+            $payload->getName()
+        ));
+    }
+
+    /**
+     * Parse a "Style" lookup response.
+     *
+     * @param  \SimpleXMLElement $payload
+     * @return Collection
+     */
+    protected function parseStyleResponse(\SimpleXMLElement $payload)
+    {
+        $collection = new Collection;
+
+        foreach ($payload->Clr as $colour) {
+            foreach ($colour->Sku as $sku) {
+                $this->parseSku($sku, $collection);
+            }
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Parse a "Clr" lookup response.
+     *
+     * @param  \SimpleXMLElement $payload
+     * @return Collection
+     */
+    protected function parseClrResponse(\SimpleXMLElement $payload)
+    {
+        $collection = new Collection;
+
+        foreach ($payload->Sku as $sku) {
+            $this->parseSku($sku, $collection);
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Parse a "Sku" lookup response.
+     *
+     * @param  \SimpleXMLElement $payload
+     * @return Collection
+     */
+    protected function parseSkuResponse(\SimpleXMLElement $payload)
+    {
+        return $this->parseSku($payload, $collection = new Collection);
+    }
+
+    /**
+     * Parse a single SKU.
+     *
+     * @param  \SimpleXMLElement $payload
+     * @param  Collection        $collection
+     * @return Collection
+     */
+    protected function parseSku(\SimpleXMLElement $payload, Collection $collection)
+    {
+        $variant = tap(new Entities\Variant, function ($variant) use ($payload) {
+            $variant->getIdentifiers()->put('ap21_id', $payload['SkuIdx']);
+        });
+
+        foreach ($payload->Store as $node) {
+
+            $store = (new Entities\Store)->setName($node['Name']);
+
+            $store->getIdentifiers()
+                ->put('ap21_id', $node['StoreId'])
+                ->put('ap21_number', $node['StoreNumber']);
+
+            $collection->push(new Entities\FreeStock($variant, $store, (int) $node['FreeStock']));
+
+        }
+
+        return $collection;
+    }
+}
