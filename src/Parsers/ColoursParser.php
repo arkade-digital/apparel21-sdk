2dcc193de7fb8ec4cb90f4e4735f264f0ada2c9d
Tejas Patel <tejas@arkade.com.au>
2017-10-09 16:01:31 +1100

    action, entities and parser added for the colours end point

---
+++ src/Parsers/ColoursParser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Arkade\Apparel21\Parsers;
+
+use SimpleXMLElement;
+use Arkade\Apparel21\Entities;
+
+class ReferenceTypeParser
+{
+    /**
+     * Parse the given SimpleXmlElement to a Colour entity.
+     *
+     * @param  SimpleXMLElement $payload
+     * @return Entities\Colour
+     */
+    public function parse(SimpleXMLElement $payload)
+    {
+        return (new Entities\Colour)
+            ->setCode((string) $payload->Code)
+            ->setName((string) $payload->Name)
+            ->setRowNumber((integer) $payload->RowNumber);
+    }
+}
